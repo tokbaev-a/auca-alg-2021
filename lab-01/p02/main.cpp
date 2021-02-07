9053b7b6b01d487c771b064bb07e8bb7d82fbebd
Arsen Tokbaev <tokbaev_a@auca.kg>
2021-02-08 05:15:59 +0600

    lab-01 p02 Done

---
+++ lab-01/p02/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+void printInDirectOrder(const list<int> &v)
+{
+    for(auto e : v)
+    {
+        cout << " " << e;
+    }
+    cout << endl;
+}
+
+void printInReverseOrder(const list<int> &v)
+{
+    for(auto i = v.rbegin(); i != v.rend(); ++i)
+    {
+        cout << " " << *i;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    list<int> v;
+
+    for(int x; cin >> x;)
+    {
+        v.push_back(x);
+    }
+
+    printInDirectOrder(v);
+    printInReverseOrder(v);
+
+    for(auto i = v.begin(); i != v.end(); ++i)
+    {
+        if(*i % 2 == 0)
+        {
+            v.insert(i, 0);
+        }
+    }
+
+    printInDirectOrder(v);
+    printInReverseOrder(v);
+
+    for(auto i = v.begin(); i != v.end();)
+    {
+        if(*i % 2 == 0)
+        {
+            i = v.erase(i);
+        }
+        else 
+        {
+            ++i;
+        }
+    }
+
+    printInDirectOrder(v);
+    printInReverseOrder(v);
+
+    v.clear();
+
+    printInDirectOrder(v);
+    printInReverseOrder(v);
+}
