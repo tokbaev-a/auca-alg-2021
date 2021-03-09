455129fbfab52986b6c5e116a24a7a8c80e1fb79
Arsen Tokbaev <tokbaev_a@auca.kg>
2021-03-10 01:12:31 +0600

    lab-02 p01 In progress

---
+++ lab-02/p01/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <iomanip>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    unordered_set<string> setOfWords;
+
+    auto hash = setOfWords.hash_function();
+
+    for (string line; getline(cin, line);)
+    {
+        istringstream sinp(line);
+
+        cout << "Bucket count: " << setOfWords.bucket_count() << "\n";
+        string cmd;
+        sinp >> cmd;
+        if (cmd == "+")
+        {
+            string word;
+            sinp >> word;
+            cout << "hash value: " << hash(word) << "\n";
+            cout << "bucket's index: " << hash(word) % setOfWords.bucket_count() << "\n";
+            auto p = setOfWords.insert(word);
+            cout << (p.second ? "YES" : "NO") << "\n";
+        }
+        else if (cmd == "?")
+        {
+            string word;
+            sinp >> word;
+            cout << "hash value: " << hash(word) << "\n";
+            cout << "bucket's index: " << hash(word) % setOfWords.bucket_count() << "\n";
+            auto it = setOfWords.find(word);
+            cout << (it != setOfWords.end() ? "YES" : "NO") << "\n";
+        }
+        else if (cmd == "-")
+        {
+            string word;
+            sinp >> word;
+            cout << "hash value: " << hash(word) << "\n";
+            cout << "bucket's index: " << hash(word) % setOfWords.bucket_count() << "\n";
+            cout << (setOfWords.erase(word) ? "YES" : "NO") << "\n";
+        }
+    }
+}
