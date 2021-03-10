b0d6bb7bb3a6dd53943af3776814173c04ea098f
Arsen Tokbaev <tokbaev_a@auca.kg>
2021-03-10 12:16:03 +0600

    project-01 uva-11849 Wrong answer

---
+++ project-01/uva-11849/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+int main()
+{
+    for (int n, m; cin >> n >> m && n + m != 0;)
+    {
+        unordered_set<int> v;
+        int x;
+
+        for (int i = 0; i < n + m; ++i)
+        {
+            cin >> x;
+            v.insert(x);
+        }
+
+        cout << (m + n - (int)v.size());
+    }
+}
