27d73f9d433ba6ac34161c4b14894a557f2e2fd5
Arsen Tokbaev <tokbaev_a@auca.kg>
2021-05-12 11:41:13 +0600

    uva-11988

---
+++ project-01/uva-11988/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <list>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    for (string in; getline(cin, in);)
+    {
+        list<char> res;
+        auto pos = res.end();
+        for (auto c : in)
+        {
+            if (c == '[')
+            {
+                pos = res.begin();
+            }
+            else if (c == ']')
+            {
+                pos = res.end();
+            }
+            else
+            {
+                res.insert(pos, c);
+            }
+        }
+        cout << string(res.begin(), res.end()) << endl;
+    }
+}
