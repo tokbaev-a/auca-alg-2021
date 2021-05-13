64899b7e0db98539e332e948cf5629d07b2f128d
Arsen Tokbaev <tokbaev_a@auca.kg>
2021-05-14 05:16:42 +0600

    lab-04 p02 Done

---
+++ lab-04/p02/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int64_t factorial(int n)
+{
+    return n == 0 ? 1 : n * factorial(n - 1);
+}
+
+int main()
+{
+    for (int n; cin >> n;)
+    {
+        cout << n << "! = " << factorial(n) << "\n";
+    }
+}
